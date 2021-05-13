a86ee74f6c9d76c2797e545435e4875fe7863dce
jeffrey <jeffrey@jeffrey.co.in>
2021-05-13 10:06:50 +0200

    first version

---
+++ script.js
@@ -0,0 +1,38 @@
+
+btn.onclick = button_handler;
+var start_time;
+var timer;
+
+const audio = new Audio("preview.mp3");
+
+function button_handler() {
+    var txt = document.getElementById("btn").textContent;
+    if(txt == "Start Timer") {
+        document.getElementById("btn").textContent= "Stop Timer";
+        start_timer();
+    }
+    else {
+        document.getElementById("btn").textContent= "Start Timer";
+        stop_timer();
+    }
+}
+
+function start_timer() {
+    start_time = Date.now();
+    timer = setInterval(run_timer, 1000); // update about every second
+}
+
+function stop_timer() {
+    clearInterval(timer); //stop the timer
+    document.getElementById("time").innerHTML = 0;
+}
+
+function run_timer() {
+    var delta = Date.now() - start_time; // milliseconds elapsed since start
+    var time = Math.floor(delta / 1000); // in seconds
+    document.getElementById("time").innerHTML = time;
+    
+    if(!(time%60)) {
+        audio.play(); // now we're safe to play it        
+    }
+}
