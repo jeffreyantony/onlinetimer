2832aa6a89f2095824434aa113b26b27dc2e2558
jeffrey <jeffrey@jeffrey.co.in>
2021-05-23 11:36:34 +0200

    basic webworker

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 
 btn.onclick = button_handler;
-var start_time;
-var timer;
 
 const audio = new Audio("hopbell.mp3");
+
+var w;
+
+function startTimerWorker() {
+    start_time = Date.now();
+    timer = setInterval(workerTimerLoop, 1000); // update about every second
+    
+    function workerTimerLoop() {
+    var delta = Date.now() - start_time; // milliseconds elapsed since start
+    var time = Math.floor(delta / 1000); // in seconds
+
+    postMessage(time);
+    }
+}
+
+function startWorker() {
+    if (typeof(Worker) !== "undefined") {
+        if (typeof(w) == "undefined") {
+            w = new Worker(URL.createObjectURL(new Blob(["("+startTimerWorker.toString()+")()"], {type: 'text/javascript'})));
+        }
+        
+        w.onmessage = function(event) {
+            document.getElementById("time").innerHTML = event.data;
+            //console.log(event.data);
+            if(!(event.data%60)) {
+                play_audio();
+                //console.log("bell");
+            }
+        };
+    }
+    else {
+        document.getElementById("time").innerHTML = "Sorry! No Web Worker support.";
+    }
+}
+
+function stopWorker() {
+  w.terminate();
+  w = undefined;
+}
 
 let volume = document.querySelector("#volume-control");
 volume.addEventListener("change", function(e) {
@@ -16,33 +53,18 @@ function button_handler() {
     var txt = document.getElementById("btn").textContent;
     if(txt == "Start Timer") {
         document.getElementById("btn").textContent= "Stop Timer";
-        start_timer();
+        startWorker();
     }
     else {
         document.getElementById("btn").textContent= "Start Timer";
-        stop_timer();
+        stopWorker();
     }
 }
 
-function start_timer() {
-    start_time = Date.now();
-    timer = setInterval(run_timer, 1000); // update about every second
-}
-
-function stop_timer() {
+/*function stop_timer() {
     clearInterval(timer); //stop the timer
     document.getElementById("time").innerHTML = 0;
-}
-
-function run_timer() {
-    var delta = Date.now() - start_time; // milliseconds elapsed since start
-    var time = Math.floor(delta / 1000); // in seconds
-    document.getElementById("time").innerHTML = time;
-    
-    if(!(time%60)) {
-        audio.play();
-    }
-}
+}*/
 
 function play_audio() {
     audio.play();
